df62c5bb5965bf11872b4ab1b3911d6f91f927d8
beata <beata@emc2wave.com>
2019-11-19 15:17:08 -0800

    create data

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'Joe Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston, MA',
+    image: 'https://randomuser.me/api/portraits/men/75.jpg'
+  },
+  {
+    name: 'Jen Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Miami, FL',
+    image: 'https://randomuser.me/api/portraits/women/75.jpg'
+  },
+  {
+    name: 'William Johnson',
+    age: 38,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Lynn, MA',
+    image: 'https://randomuser.me/api/portraits/men/76.jpg'
+  },
+]
